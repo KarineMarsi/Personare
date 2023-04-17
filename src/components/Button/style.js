c06161f988325c78b4aaf3cbad8cd6ade7a1e9bd
Karine <Karine.marsi@gmail.com>
2023-04-17 16:55:14 -0300

    Personare finalizado

---
+++ src/components/Button/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const Button = styled.button`
+background-position: center;
+background-size: cover;
+background-color: #483D8B;
+border: none;
+padding: 10px 20px;
+font-size: 20px;
+border-radius: 20px;
+color: #ffffff;
+font-family :Roboto;
+`
